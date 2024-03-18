3482a50beebc832139c3ee7b55e7e5e98f22db9f
abidmehmod <160239077+abidmehmod@users.noreply.github.com>
2024-03-18 22:39:23 +0500

    adding folder

---
+++ add.ts
@@ -0,0 +1,6 @@
+//exercise no:10
+//Abid mehmood
+//date : 2-19-2024
+// this program show hello world
+
+console.log('hello world simple program');
---
+++ prog2.ts
@@ -0,0 +1,7 @@
+//exercise no:10
+//Abid mehmood
+//date : 2-19-2024
+// this program show addition
+
+
+console.log(3 + 2);
